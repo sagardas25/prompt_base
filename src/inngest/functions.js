d61d064008685d98fa075e85919f02b52e00aa0f
sagardas25 <sagar.xyz05@gmail.com>
2025-12-15 23:53:32 +0530

    configure e2b sanbox

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -1,22 +1,37 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import Sandbox from "@e2b/code-interpreter";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "agent/hello" },
   async ({ event, step }) => {
 
-    const helloAgent = createAgent({
-      name:"hello-agent",
-      description:"A simple agent that say hello",
-      system:"You're a friendly AI assistant that greets users.",
-      model:gemini({model:"gemini-2.5-flash"})
-    })
+    const sandBoxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("prompt-base-v0");
+      // use sandboxId not sandBoxId
+      return sandbox.sandboxId;
+    });
 
-    const {output} = await helloAgent.run("Say Hello to the user!");
+
+
+    const helloAgent = createAgent({
+      name: "hello-agent",
+      description: "A simple agent that say hello",
+      system: "you are a friendly assistant that greets user with enthusiasm",
+      model: gemini({ model: "gemini-2.5-flash" }),
+    });
+
+    const { output } = await helloAgent.run("Say Hello to the user!");
+
+    const sandBoxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await Sandbox.connect(sandBoxId);
+      const host = sandbox.getHost(3000);
+      return `http://${host}`;
+    });
 
     return {
-      message: output[0].content
-    }
+      message: output[0].content,
+    };
   }
 );
